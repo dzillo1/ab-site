1deac1a12470d4853b1320cee51a559d83da2e56
andrew borzillo <dzillo1@gmail.com>
2025-04-28 15:59:05 -0400

    edit

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -8,7 +8,6 @@ function App() {
   return (
     <>
       <Header/>
-      <p>Hello, this is a test!</p>
     </>
   )
 }
--- portfolio/src/Components/Header.jsx
+++ portfolio/src/Components/Header.jsx
@@ -1,7 +1,8 @@
 function Header(){
     return(
         <header>
-            <h1>Portfolio</h1>
+            <h1>Andrew Borzillo</h1>
+            <h2><em>Front End Developer</em></h2>
         </header>
         )
 }
